21687d095ba025056d7c01e107251d7dc9143d7f
olicendr <olicendr@gmail.com>
2021-09-26 20:59:10 +0300

    local storage

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -9,14 +9,21 @@ export class CartService {
   cartItems: Item[] = [];
 
   getCartItems() {
+    let localData: any = window.localStorage.getItem("cartItems");
+
+    if (JSON.parse(localData).length) {
+      this.cartItems = JSON.parse(localData);
+    }
     return this.cartItems;
   }
 
   addToCart(item: Item) {
     this.cartItems.push(item);
+    localStorage.setItem("cartItems", JSON.stringify(this.cartItems));
   }
 
   removeFromCart(removedItem: Item) {
     this.cartItems = this.cartItems.filter(item => item.title !== removedItem.title);
+    localStorage.setItem("cartItems", JSON.stringify(this.cartItems));
   }
 }
--- src/app/favorites.service.ts
+++ src/app/favorites.service.ts
@@ -9,14 +9,21 @@ export class FavoritesService {
   favoritesItems: Item[] = [];
 
   getFavoriteItems() {
+    let localData: any = window.localStorage.getItem("favoriteItems");
+
+    if (JSON.parse(localData).length) {
+      this.favoritesItems = JSON.parse(localData);
+    }
     return this.favoritesItems;
   }
 
   addToFavorites(item: Item) {
-    this.favoritesItems.push(item)
+    this.favoritesItems.push(item);
+    localStorage.setItem("favoriteItems", JSON.stringify(this.favoritesItems));
   }
 
   removeFromFavorites(removedItem: Item) {
-    this.favoritesItems = this.favoritesItems.filter(item => item.title === removedItem.title)
+    this.favoritesItems = this.favoritesItems.filter(item => item.title !== removedItem.title);
+    localStorage.setItem("favoriteItems", JSON.stringify(this.favoritesItems));
   }
 }
